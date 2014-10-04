47d6d1163fa092699f82c11ebc6127b1cde084c1
Tong Jia <tongjia19@gmail.com>
2014-10-03 23:30:08 -0700

    Adding script to execute single or all prob.

---
+++ run.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Compile and run a particular problem
+# Tong Jia
+# 2014
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 [PROGRAMMING LANGUAGE] [PROBLEM #]";
+  exit 1
+fi
+
+case "$1" in
+        c)
+            file_ext="c"
+            language_name="C"
+            compiler="gcc"
+            ;;
+         
+        *)
+            echo -e "\"$1\" not found!"
+            exit 1
+esac
+
+echo -e "Language: $language_name"
+echo -e "=========================\n"
+
+cd $1
+
+function compile {
+  printf "${prob_file}:  "
+  printf "compiling.. "
+  ${compiler} pe_p${prob_num}.${file_ext} -o pe_p${prob_num}.bin
+}
+
+function execute {
+  printf "executing:  "
+  ./pe_p${prob_num}.bin
+
+  echo -e ""
+  echo -e "-------------------------\n"
+}
+
+function clean {
+  rm *.bin
+}
+
+if [ "$2" = "all" ]; then
+  for prob_file in $( ls ); do
+    [[ "${prob_file}" =~ pe_p(.*).${file_ext} ]]
+    prob_num=${BASH_REMATCH[1]}    
+    compile
+    execute
+  done
+else
+  prob_num=$2
+  prob_file=pe_p${prob_num}.${file_ext}
+  compile
+  execute
+fi
+
+clean
+
