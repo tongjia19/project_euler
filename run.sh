f716fee0ace58c1facf1ab94e3563c6fe8623597
Tong Jia <tongjia19@gmail.com>
2014-10-04 18:56:49 -0700

    Fixing indents. Slight style change when outputting.

--- run.sh
+++ run.sh
@@ -5,20 +5,20 @@
 # 2014
 
 if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 [PROGRAMMING LANGUAGE] [PROBLEM #]";
-  exit 1
+    echo "Usage: $0 [PROGRAMMING LANGUAGE] [PROBLEM #]";
+    exit 1
 fi
 
 case "$1" in
-        c)
-            file_ext="c"
-            language_name="C"
-            compiler="gcc"
-            ;;
-         
-        *)
-            echo -e "\"$1\" not found!"
-            exit 1
+    c)
+        file_ext="c"
+        language_name="C"
+        compiler="gcc"
+        ;;
+
+    *)
+        echo -e "\"$1\" not found!"
+        exit 1
 esac
 
 echo -e "Language: $language_name"
@@ -27,35 +27,36 @@ echo -e "=========================\n"
 cd $1
 
 function compile {
-  printf "${prob_file}:  "
-  printf "compiling.. "
-  ${compiler} pe_p${prob_num}.${file_ext} -o pe_p${prob_num}.bin
+    printf "${prob_file}:  "
+    printf "compiling.. "
+    ${compiler} pe_p${prob_num}.${file_ext} -o pe_p${prob_num}.bin
 }
 
 function execute {
-  printf "executing:  "
-  ./pe_p${prob_num}.bin
+    printf "executing:  "
+    ./pe_p${prob_num}.bin
 
-  echo -e ""
-  echo -e "-------------------------\n"
+    echo -e ""
+    echo -e "-------------------------\n"
 }
 
 function clean {
-  rm *.bin
+    rm *.bin
 }
 
 if [ "$2" = "all" ]; then
-  for prob_file in $( ls ); do
-    [[ "${prob_file}" =~ pe_p(.*).${file_ext} ]]
-    prob_num=${BASH_REMATCH[1]}    
+    for prob_file in $( ls ); do
+        [[ "${prob_file}" =~ pe_p(.*).${file_ext} ]]
+        prob_num=${BASH_REMATCH[1]}
+        printf "${prob_num}: "
+        compile
+        execute
+    done
+else
+    prob_num=$2
+    prob_file=pe_p${prob_num}.${file_ext}
     compile
     execute
-  done
-else
-  prob_num=$2
-  prob_file=pe_p${prob_num}.${file_ext}
-  compile
-  execute
 fi
 
 clean
